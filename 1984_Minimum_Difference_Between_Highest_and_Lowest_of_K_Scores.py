724f68ebaa80f7720bb848a74dd6cdea37e39925
Aryan Borasi <aryanborasi179@gmail.com>
2026-01-25 11:57:21 +0530

    Added Leetcode 1877 solution

---
+++ 1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores.py
@@ -0,0 +1,14 @@
+# LeetCode 1877: Minimize Maximum Pair Sum in Array
+# Difficulty: Easy
+# Topics: Array, Sliding window
+# Time Complexity: O(n log n)
+# Space Complexity: O(1)
+class Solution(object):
+    def minimumDifference(self, nums, k):
+        if k==1:
+            return 0
+        nums.sort()
+        res=float('inf')
+        for i in range(len(nums)-k+1):
+                res=min(res,nums[i+k-1]-nums[i])    
+        return res
